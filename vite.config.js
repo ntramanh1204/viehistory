f14eacaccc6f4b95f20edc0c1e53bb25fd742808
ntramanh1204 <ntramanh1204@gmail.com>
2025-06-18 12:07:20 +0700

    Add Cloudflare config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                // blog: resolve(__dirname, 'public/blog.html'),
-                // forum: resolve(__dirname, 'public/forum.html'),
-                // login: resolve(__dirname, 'public/login.html')
-            }
-        }
-    },
-    server: {
-        port: 3000,
-        open: 'index.html'
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src')
-            // '@components': resolve(__dirname, 'src/components'),
-            // '@services': resolve(__dirname, 'src/services'),
-            // '@styles': resolve(__dirname, 'src/styles')
-        }
-    },
-    publicDir: 'public'
-});
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
